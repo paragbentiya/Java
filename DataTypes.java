7244f9ba831fbf0571eafc0ee6a97c7a414c310d
Parag Jain <98089828+paragbentiya@users.noreply.github.com>
2023-09-12 01:24:16 +0530

    Add files via upload
    Data Types

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+public static void main(String[] args)
+{
+int a=100;
+float f=100.00f;
+byte b=5;
+double d=10000;
+char c='A';
+short s=12;
+long l=12323;
+boolean bool=true;
+System.out.println(a+" "+f+" "+b+" "+d+" "+c+" "+s+" "+l+" "+bool);
+}
+}
